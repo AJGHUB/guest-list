9f86265d04fb0c4eef8d9aff47328393401efc2c
Alice Jowsey <12345alcatraz@gmail.com>
2020-08-20 17:54:36 +1200

    add stylesheet from github

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './stylesheet/App.css';
+import { 
+   BrowserRouter,
+   Switch,
+   Route,
+   } from 'react-router-dom';
+
+import './stylesheet/stylesheet.css';
 import { Home } from './components/Home';
 import { AddGuest } from './components/AddGuest';
 import { EditGuest } from './components/EditGuest';
-
 
 import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
     <GlobalProvider>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/add" component={AddGuest} exact />
-        <Route path="/edit/:id" component={EditGuest} exact />
-      </Switch>
+      <BrowserRouter>
+          <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/add" component={AddGuest} exact />
+              <Route path="/edit/:id" component={EditGuest} exact />
+          </Switch>
+        </BrowserRouter>
     </GlobalProvider>
   );
 }
